f19997909c60bb0f50961164d695c64bfc25ad1b
Artem <archyz@gmail.com>
2023-07-06 21:02:26 +0300

    working thread

--- src/try.py
+++ src/try.py
@@ -2,6 +2,7 @@ from cmd import Cmd
 import asyncio
 from threading import Thread
 from time import sleep
+import readline
 
 
 def echo(line):
@@ -11,45 +12,34 @@ def echo(line):
 def arun(line):
   for i in range(int(line)):
     sleep(2)
+    print()
     print(echo(i))
+    print('> ', readline.get_line_buffer(), sep='', end='', flush=True)
 
 
 class Run(Cmd):
   intro = 'Welcome to the turtle shell.   Type help or ? to list commands.\n'
-  prompt = '(turtle) '
-  
-  async def echo(self, line):
-    return line
-
-
-  async def arun(self, line):
-    for i in range(int(line)):
-      await asyncio.sleep(2)
-      self.stdout.write(await self.echo(i))
+  prompt = '> '
   
 
 
-  def __init__(self, loop: asyncio.AbstractEventLoop, completekey: str = "tab") -> None:
+  def __init__(self, completekey: str = "tab", prompt: str='> ') -> None:
     super().__init__(completekey)
-    self.loop = loop
+    self.prompt = prompt
   
   
   def do_one(self, line):
     print(f'Cmd = {line}')
-    
-    
-  def do_run(self, line):
-    bt = set()
-    task = self.loop.create_task(self.arun(line))
-    bt.add(task)
-    task.add_done_callback(bt.discard)
-      
+
 
   def do_trun(self, line):
     th = Thread(target=arun, daemon=True, name='BG', kwargs={'line': line})
     th.start()
-    
-    
-loop = asyncio.new_event_loop()
-acmd = Run(loop=loop)
+
+
+  def do_EOF(self, line):
+    return True
+
+
+acmd = Run()
 acmd.cmdloop()
